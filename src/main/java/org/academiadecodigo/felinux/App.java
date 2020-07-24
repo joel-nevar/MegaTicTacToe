be51cf23db255a1d7c759288ab5e766f4bffb895
bernardocosta <bernardo.costa.90@gmail.com>
2020-07-24 20:52:53 +0100

    solve some issues with Maven or else

--- src/main/java/org/academiadecodigo/felinux/App.java
+++ src/main/java/org/academiadecodigo/felinux/App.java
@@ -1,8 +1,10 @@
 package org.academiadecodigo.felinux;
 
+import org.academiadecodigo.bootcamp.Prompt;
 import org.academiadecodigo.felinux.mvc.model.cell.Cell;
 
 public class App {
     public static void main(String[] args) {
+        
     }
 }
