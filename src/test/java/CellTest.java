17723e027d73ac82f8fe1aaf780c588b1c29dab9
Pikachu <davidmrferreira@hotmail.com>
2020-07-24 22:22:17 +0100

    Cell builder implemented

--- src/test/java/CellTest.java
+++ src/test/java/CellTest.java
@@ -1,10 +1,8 @@
 import org.academiadecodigo.felinux.mvc.model.cell.Cell;
+import org.academiadecodigo.felinux.mvc.model.cell.CellValueType;
 
 public class CellTest {
     public static void main(String[] args) {
-
-        Cell cell = new Cell("a", "b");
-        cell.drawCellInNewLine();
-        cell.drawCellInSameLine(cell);
+        Cell cellBuilder = new Cell.CellBuilder("a", "b", CellValueType.EMPTY).setDrawCellInNewLine().build();
     }
 }
