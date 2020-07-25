ae5c25e6ae97a7d3dda2db01e0e5090fffdea270
Eddie <emarques07@gmail.com>
2020-07-25 01:47:31 +0100

    Player register & create new rooms

--- src/main/java/org/academiadecodigo/felinux/mvc/controller/CentralController.java
+++ src/main/java/org/academiadecodigo/felinux/mvc/controller/CentralController.java
@@ -2,11 +2,13 @@ package org.academiadecodigo.felinux.mvc.controller;
 
 import org.academiadecodigo.felinux.mvc.model.Lobby;
 import org.academiadecodigo.felinux.mvc.model.PlayerHandler;
+import org.academiadecodigo.felinux.service.PlayerService;
 
 public class CentralController implements Controller{
 
 
     private Lobby lobby;
+    private PlayerService playerService;
 
 
     @Override
@@ -14,10 +16,14 @@ public class CentralController implements Controller{
 
     }
     public void registerPlayer(PlayerHandler player){
-
+        playerService.registerPlayer(player);
     }
 
     public void setLobby(Lobby lobby) {
         this.lobby = lobby;
+    }
+
+    public void setPlayerService(PlayerService playerService) {
+        this.playerService = playerService;
     }
 }
