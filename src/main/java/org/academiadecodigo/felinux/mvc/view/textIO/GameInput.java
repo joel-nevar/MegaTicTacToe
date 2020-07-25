79f958895821032c9acd5fdecdb525c7a820f95e
bernardocosta <bernardo.costa.90@gmail.com>
2020-07-25 16:54:49 +0100

    add game inputs

---
+++ src/main/java/org/academiadecodigo/felinux/mvc/view/textIO/GameInput.java
@@ -0,0 +1,21 @@
+package org.academiadecodigo.felinux.mvc.view.textIO;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GameInput {
+
+    public static final Set<String> cellCoordinates = new HashSet<String>() {
+        { 
+            add("A1");
+            add("A2");
+            add("A3");
+            add("B1");
+            add("B2");
+            add("B3");
+            add("C1");
+            add("C2");
+            add("C3");
+        }
+    };
+}
--- src/main/java/org/academiadecodigo/felinux/mvc/view/textIO/Messages.java
+++ src/main/java/org/academiadecodigo/felinux/mvc/view/textIO/Messages.java
@@ -1,11 +1,11 @@
-package org.academiadecodigo.felinux.mvc.view;
+package org.academiadecodigo.felinux.mvc.view.textIO;
 
 public class Messages {
 
-    protected static String MENU_MESSAGE = "Welcome to Mega TicTacToe! Please select:";
-    protected static String VIEW_MAIN_ERROR = "Item does not exit";
+    public static String MENU_MESSAGE = "Welcome to Mega TicTacToe! Please select:";
+    public static String VIEW_MAIN_ERROR = "Item does not exit";
 
-    protected static String[] getMenuMessages() {
+    public static String[] getMenuMessages() {
 
         String[] menuStrings = new String[MenuOptions.values().length];
 
@@ -17,7 +17,7 @@ public class Messages {
         return menuStrings;
     }
 
-    protected enum MenuOptions {
+    public enum MenuOptions {
         SIMPLE_VS_COM("Single Player"),
         SIMPLE_VS_PLAYER("1 vs 1"),
         MEGA_VS_COM("Mega Single Player"),
