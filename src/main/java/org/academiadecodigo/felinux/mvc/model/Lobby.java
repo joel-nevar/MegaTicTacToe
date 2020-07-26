213edd2cb5d20ed47ee636b0db850570d5cdee97
Eddie <emarques07@gmail.com>
2020-07-26 12:27:20 +0100

    grid in rooms

--- src/main/java/org/academiadecodigo/felinux/mvc/model/Lobby.java
+++ src/main/java/org/academiadecodigo/felinux/mvc/model/Lobby.java
@@ -1,15 +1,17 @@
 package org.academiadecodigo.felinux.mvc.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Lobby {
 
     private ArrayList <Room> rooms;
+    private HashSet<PlayerHandler> players;
 
     public Lobby(){
 
         rooms = new ArrayList<>();
-
+        players = new HashSet<>();
     }
 
     public void addRoom(Room room){
